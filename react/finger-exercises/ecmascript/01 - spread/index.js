d9ed802647ffd8f216da77e0efea79ac39218d49
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-12-04 16:48:13 -0300

    Fixed tests

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,10 +1,32 @@
+/* eslint-disable no-console */
 import { isArray } from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(...args) {
+  if (!isArray(args)) {
+    return args;
+  } else if (args.length === 0) {
+    return undefined;
+  } else if (args.length === 1) {
+    if (isArray(args[0])) {
+      return Math.min(...(args[0]));
+    }
+    return Math.min(args[0]);
+  }
+  return Math.min(...args);
 }
 
-export function copy() {
+export function copy(args) {
+  if (isArray(args)) {
+    return [...args];
+  }
+  return { ...args };
+}
 
+export function reverseMerge(firstArray, lastArray) {
+  return [...lastArray, ...firstArray];
+}
+
+export function filterAttribs(args) {
+  const { a, b, ...rest } = args;
+  return rest;
 }
--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,19 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const start = Date.now();
+  return new Promise((resolve, reject) => {
+    if (time < 1000) {
+      setTimeout(() => {
+        const end = Date.now();
+        resolve(end - start);
+      }, time);
+    } else {
+      reject(new Error('This time is too much !'));
+    }
+  });
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(time) {
+  return delay(time);
 }
