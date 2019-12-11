e4e13fae2db68a9a998185a7b6ebe52e2597a071
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2019-12-11 18:36:44 -0300

    books listed

--- react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
+++ react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-console */
 import React, { Component, Fragment } from 'react';
-import store from '@redux/store';
+import { connect } from 'react-redux';
 import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
+import { func, arrayOf } from 'prop-types';
+import { bookPropType } from '@constants/propTypes';
+
+import actionsCreators from '../../../redux/book/actions';
 
 import Book from './components/Book';
 import Search from './components/Search';
@@ -9,30 +14,29 @@ import ShoppingCart from './components/ShoppingCart';
 import styles from './styles.scss';
 
 class App extends Component {
-  state = {
-    books: [],
-    bookSelected: []
-  };
-
   componentDidMount() {
-    store.subscribe(() => {
-      const { books, bookSelected } = store.getState();
-      this.setState({ books, bookSelected });
-    });
-    // TODO to implement the dispatch
+    this.props.getBooks();
   }
 
   // TODO to implement the dispatch
-  onSearch = value => {};
+  onSearch = value => {
+    console.log(value);
+  };
 
   // TODO to implement the dispatch
-  addToCart = item => {};
+  addToCart = item => {
+    console.log(item);
+  };
 
   // TODO to implement the dispatch
-  addItem = itemId => {};
+  addItem = itemId => {
+    console.log(itemId);
+  };
 
   // TODO to implement the dispatch
-  removeItem = itemId => {};
+  removeItem = itemId => {
+    console.log(itemId);
+  };
 
   CONFIGURATION_BUTTON = {
     add: {
@@ -47,7 +51,7 @@ class App extends Component {
   };
 
   renderBooks = item => {
-    const showButton = !this.state.bookSelected.some(el => el.id === item.id);
+    const showButton = !this.props.bookSelected.some(el => el.id === item.id);
     const configButton = showButton ? this.CONFIGURATION_BUTTON.add : this.CONFIGURATION_BUTTON.remove;
     return <Book key={item.id} data={item} configButton={configButton} />;
   };
@@ -58,16 +62,16 @@ class App extends Component {
         <Navbar />
         <div className={styles.container}>
           <Search onSearch={this.onSearch} />
-          {this.state.books.length ? (
-            this.state.books.map(this.renderBooks)
+          {this.props.books.length ? (
+            this.props.books.map(this.renderBooks)
           ) : (
             <div className={styles.noData}>
               <h2 className={styles.title}>No Data</h2>
             </div>
           )}
         </div>
-        {this.state.bookSelected.length ? (
-          <ShoppingCart data={this.state.bookSelected} addItem={this.addItem} removeItem={this.removeItem} />
+        {this.props.bookSelected.length ? (
+          <ShoppingCart data={this.props.bookSelected} addItem={this.addItem} removeItem={this.removeItem} />
         ) : null}
         <Footer />
       </Fragment>
@@ -75,4 +79,19 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  books: state.books,
+  bookSelected: state.bookSelected
+});
+
+const mapDispatchToProps = dispatch => ({
+  getBooks: () => dispatch(actionsCreators.getBooks())
+});
+
+App.propTypes = {
+  getBooks: func,
+  books: arrayOf(bookPropType),
+  bookSelected: bookPropType
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
