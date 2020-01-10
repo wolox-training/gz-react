323b519ae38d2efd75f2812661f96be8f111e727
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2020-01-10 16:49:45 -0300

    Added players view

--- react/tic-tac-toe/src/app/screens/Players/index.js
+++ react/tic-tac-toe/src/app/screens/Players/index.js
@@ -6,6 +6,10 @@ import getMatches from '../../../services/MatchesService';
 import actionsCreators from '../../../redux/match/actions';
 import TopBar from '../Game/components/TopBar';
 import { matchPropType } from '../../../constants/propTypes';
+import { PLAYER_ONE, TIE } from '../../../constants/gameConstants';
+import { history } from '../../../redux/store';
+
+import styles from './styles.module.scss';
 
 
 class Players extends Component {
@@ -18,31 +22,43 @@ class Players extends Component {
   }
 
   getPlayersMatches() {
-    const players = {
-      playerOne: { won: 0, tie: 0, lost: 0 },
-      playerTwo: { won: 0, tie: 0, lost: 0 }
-    };
+    const players = [{ won: 0, tie: 0, lost: 0 }, { won: 0, tie: 0, lost: 0 }];
     this.props.matchHistory.forEach((match) => {
-      if (match.winner === 'tie') {
-        players.playerOne.tie++;
-        players.playerTwo.tie++;
-      } else if (match.winner === 'player_one') {
-        players.playerOne.won++;
-        players.playerTwo.lost++;
+      if (match.winner === TIE) {
+        players[0].tie++;
+        players[1].tie++;
+      } else if (match.winner === PLAYER_ONE) {
+        players[0].won++;
+        players[1].lost++;
       } else {
-        players.playerOne.lost++;
-        players.playerTwo.won++;
+        players[0].lost++;
+        players[1].won++;
       }
     });
     return players;
+  }
+
+  renderPlayers() {
+    const players = this.getPlayersMatches();
+    return players.map((item, i) => (
+      <tr className={styles.playerRow} key={`match_${i + 1}`}>
+        <td>{`Player ${i + 1}`}</td>
+        <td>{item.won}</td>
+        <td>{item.tie}</td>
+        <td>{item.lost}</td>
+      </tr>
+    ));
+  }
+
+  handleGoBack() {
+    history.goBack();
   }
 
   render () {
     return (
       <Fragment>
         <TopBar />
-        <h2>Players</h2>
-        <table>
+        <table className={styles.playersTable}>
           <thead>
             <tr>
               <th>Name</th>
@@ -51,8 +67,11 @@ class Players extends Component {
               <th>Lost</th>
             </tr>
           </thead>
-          <tbody />
+          <tbody>
+            {this.renderPlayers()}
+          </tbody>
         </table>
+        <button className={styles.goBack} type="button" onClick={this.handleGoBack}>Back</button>
       </Fragment>);
   }
 }
