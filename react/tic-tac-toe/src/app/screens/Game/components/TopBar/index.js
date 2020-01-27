a0fb99e26f00d09f4b1fdc6f8d2c8ef82f044b51
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2020-01-27 11:12:04 -0300

    Fixes

--- react/tic-tac-toe/src/app/screens/Game/components/TopBar/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/TopBar/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { func } from 'prop-types';
 
 import actionsCreators from '../../../../../redux/users/actions';
+import { ROUTES } from '../../../../../constants/gameConstants';
 
 import styles from './styles.module.scss';
 
@@ -14,8 +15,8 @@ class TopBar extends Component {
   render() {
     return (
       <ul className={`row ${styles.navbar}`}>
-        <li className={styles.menuOption}><Link to="/game">Game</Link></li>
-        <li className={styles.menuOption}><Link to="/players">Players</Link></li>
+        <li className={styles.menuOption}><Link to={ROUTES.GAME}>Game</Link></li>
+        <li className={styles.menuOption}><Link to={ROUTES.PLAYERS}>Players</Link></li>
         <li className={styles.menuOption}><button type="button" onClick={this.handleClick}>Logout</button></li>
       </ul>
     );
